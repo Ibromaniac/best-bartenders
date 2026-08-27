8f73a0c77acf8676513cd1f2e15666e1a0edbd0c
Ibromaniac <ibromaniac@gmail.com>
2026-08-27 05:05:47 -0700

    Switch email sending to Resend

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,14 +1,26 @@
-const sgMail = require("@sendgrid/mail");
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 async function sendEmail({ to, subject, html }) {
-  await sgMail.send({
-    to,
-    from: process.env.EMAIL_FROM,
-    subject,
-    html
+  const response = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      from: process.env.EMAIL_FROM,
+      to: [to],
+      subject,
+      html,
+    }),
   });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    console.error("RESEND EMAIL ERROR:", data);
+    throw new Error(data.message || "Failed to send email");
+  }
+
+  return data;
 }
 
 module.exports = sendEmail;
